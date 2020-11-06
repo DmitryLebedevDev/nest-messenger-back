706e56253d852d111cd390bfab9125d17bb8848c
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2020-11-06 22:11:57 +0800

    create checkError class

--- src/common/check.ts
+++ src/common/check.ts
@@ -1,3 +1,13 @@
+import { ERROR_MESSAGES } from "./ERROR_MESSAGES";
+import { HttpStatus } from "@nestjs/common";
+
+export class CheckError {
+  constructor(public error: string,
+              public status: number = HttpStatus.BAD_REQUEST,
+              public message: string = ERROR_MESSAGES.BAD_REQUEST
+  ) {}
+}
+
 export const check = (any: any, errorMessage: string) => {
   if (any)
     throw new Error(errorMessage)
