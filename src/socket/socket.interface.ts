3eafee792d325e7680ced9ec39a1080f77fffaea
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-06-03 10:55:31 +0800

    add types

--- src/socket/socket.interface.ts
+++ src/socket/socket.interface.ts
@@ -4,3 +4,14 @@ import { IjwtUser } from '../user/interface/user.interface';
 export interface SocketWithUser extends Socket {
   user: IjwtUser
 }
+
+interface Iconnect {
+  to: string
+  from: string
+  desc: RTCSessionDescription
+}
+interface IconnectIces {
+  to: string,
+  from: string,
+  ices: RTCIceCandidateInit[]
+}
